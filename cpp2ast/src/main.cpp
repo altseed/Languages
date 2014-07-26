a3a8a1abf8367275debd97bd193f971806192af1
ArGxento <argxento@gmail.com>
2014-07-26 15:23:54 +0900

    コンパイルを通るようにコード、設定等追加

--- cpp2ast/src/main.cpp
+++ cpp2ast/src/main.cpp
@@ -1,6 +1,43 @@
+/*
+cpp2ast: make AST from C++ code by clang.
+
+This project is distributed under the University of Illinois Open Source License.
+
+*/
+#include <string>
 #include <iostream>
 #include <clang-c/Index.h>
-int main() {
+
+
+std::string getSpell(CXCursor const& cur) {
+	CXString spell = clang_getCursorSpelling(cur);
+	std::string const ret = std::string(clang_getCString(spell));
+	clang_disposeString(spell);
+	return ret;
+}
+
+CXChildVisitResult walkerCallback(CXCursor cursor, CXCursor parent, CXClientData client_data) {
+
+	std::cout << getSpell(cursor) << std::endl;
+
+	return CXChildVisit_Recurse;
+}
+
+int main(int argc, char *argv[]) {
 	
+
+	auto idx = clang_createIndex(1, 1);
+	auto trans = clang_createTranslationUnit(idx, argv[1]);
+
+	if(nullptr == trans) {
+		return -1;
+	}
+	auto cur = clang_getTranslationUnitCursor(trans);
+	clang_visitChildren(cur, walkerCallback, nullptr);
+	clang_disposeIndex(idx);
+	clang_disposeTranslationUnit(trans);
+	char c;
+	std::cin >>c;
+
 	return 0;
 }
