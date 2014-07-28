c122ed1511de62512c3c7de26417d7b9110dd914
durswd <swda.dual@gmail.com>
2014-07-28 11:26:56 +0900

    ロズリンのサンプルを追加

---
+++ cs2ast/bin/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs2ast
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if(args.Length == 0)
+			{
+				Console.WriteLine("Hello World");
+			}
+		}
+	}
+}
---
+++ cs2ast/cs2ast/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs2ast
+{
+	class Walker : Microsoft.CodeAnalysis.SyntaxWalker
+	{
+		public override void Visit(Microsoft.CodeAnalysis.SyntaxNode node)
+		{
+			if (node != null)
+			{
+				Console.WriteLine("[Node  - Type: {0}, Kind: {1}]\n{2}\n", node.GetType().Name, node.RawKind, node);
+			}
+			
+			base.Visit(node);
+		}
+
+		protected override void VisitToken(Microsoft.CodeAnalysis.SyntaxToken token)
+		{
+			base.VisitToken(token);
+		}
+
+		protected override void VisitTrivia(Microsoft.CodeAnalysis.SyntaxTrivia trivia)
+		{
+			base.VisitTrivia(trivia);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var code = System.IO.File.ReadAllText("Program.cs");
+
+			var syntaxTree = Microsoft.CodeAnalysis.CSharp.CSharpSyntaxTree.ParseText(code);
+			var rootNode = syntaxTree.GetRoot();
+			var walker = new Walker();
+			walker.Visit(rootNode);
+		}
+	}
+}
