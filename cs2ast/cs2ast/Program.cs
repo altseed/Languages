6d6a9d213bddaff3dfead08c2a7ca55231761752
ArGxento <argxento@gmail.com>
2014-08-16 14:42:00 +0900

    長い名前空間をusingに変更、意味解析できるようにしてシンボルの宣言名を取得するように

--- cs2ast/cs2ast/Program.cs
+++ cs2ast/cs2ast/Program.cs
@@ -3,27 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 namespace cs2ast
 {
-	class Walker : Microsoft.CodeAnalysis.SyntaxWalker
+	class Walker : SyntaxWalker
 	{
-		public override void Visit(Microsoft.CodeAnalysis.SyntaxNode node)
+		private SemanticModel sema;
+		public Walker(SemanticModel s) { sema = s; }
+		public override void Visit(SyntaxNode node)
 		{
 			if (node != null)
 			{
-				Console.WriteLine("[Node  - Type: {0}, Kind: {1}]\n{2}\n", node.GetType().Name, node.RawKind, node);
+				Console.WriteLine("[Node  - Type: {0}, Kind: {1}]\n{2}\n", node.GetType().Name, ((SyntaxKind)node.RawKind).ToString(), node);
+
+				var si = sema.GetSymbolInfo(node);
+				var n = si.Symbol;
+				if (n != null)
+				{
+					Console.WriteLine("Definition: {0}\n", n.OriginalDefinition.ToDisplayString());
+				}
+
 			}
-			
+
+
 			base.Visit(node);
 		}
 
-		protected override void VisitToken(Microsoft.CodeAnalysis.SyntaxToken token)
+		protected override void VisitToken(SyntaxToken token)
 		{
 			base.VisitToken(token);
 		}
 
-		protected override void VisitTrivia(Microsoft.CodeAnalysis.SyntaxTrivia trivia)
+		protected override void VisitTrivia(SyntaxTrivia trivia)
 		{
 			base.VisitTrivia(trivia);
 		}
@@ -35,27 +47,32 @@ namespace cs2ast
 		{
 			var code = System.IO.File.ReadAllText("Program.cs");
 
-			var syntaxTree = Microsoft.CodeAnalysis.CSharp.CSharpSyntaxTree.ParseText(code);
+			var syntaxTree = CSharpSyntaxTree.ParseText(code);
 			var rootNode = syntaxTree.GetRoot();
-			var walker = new Walker();
-			walker.Visit(rootNode);
+
 
 			var assemblyPath = System.IO.Path.GetDirectoryName(typeof(object).Assembly.Location);
-			var mscorelib = new Microsoft.CodeAnalysis.MetadataFileReference(System.IO.Path.Combine(assemblyPath, "mscorlib.dll"));
+			var mscorelib = new MetadataFileReference(System.IO.Path.Combine(assemblyPath, "mscorlib.dll"));
 
 			var compilation = Microsoft.CodeAnalysis.CSharp.CSharpCompilation.Create(
 						"Compilation",
 						syntaxTrees: new[] { syntaxTree },
 						references: new[] { mscorelib },
-						options : new Microsoft.CodeAnalysis.CSharp.CSharpCompilationOptions(
-                                                  Microsoft.CodeAnalysis.OutputKind.ConsoleApplication));
+						options: new Microsoft.CodeAnalysis.CSharp.CSharpCompilationOptions(
+												  Microsoft.CodeAnalysis.OutputKind.ConsoleApplication));
 
 			var semanticModel = compilation.GetSemanticModel(syntaxTree);
+
 			var decl = semanticModel.GetDeclarationDiagnostics();
 			var methodBodies = semanticModel.GetMethodBodyDiagnostics();
 
 			var type = compilation.GetTypeByMetadataName("cs2ast.Program");
-			
+
+			var walker = new Walker(semanticModel);
+			walker.Visit(semanticModel.SyntaxTree.GetRoot());
+
+			System.Console.ReadKey(false);
+
 		}
 	}
 }
